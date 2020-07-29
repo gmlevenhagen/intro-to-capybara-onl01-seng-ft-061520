fc6c4c9e37cd17233e61dd97e31f3b07181f0289
gmlevenhagen <garettlevenhagen@gmail.com>
2020-07-29 10:35:57 -0500

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,3 +18,12 @@ end
 
 # Configure Capybara to test against the application above.
 Capybara.app = app
+
+require 'spec_helper'
+ 
+describe "GET '/' - Greeting Form" do
+  it 'welcomes the user' do
+    visit '/'
+    expect(page.body).to include("Welcome!")
+  end
+end
